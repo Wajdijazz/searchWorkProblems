c20bbc39c44bbf668ed273fcb0e8b2f3835ea151
Wajdijazz <mohamedwajdijaziri@gmail.com>
2021-11-02 13:13:25 +0200

    add tow pb

---
+++ TreeBinery/Tree.java
@@ -0,0 +1,15 @@
+class Node {
+	Node left,right;
+	int value;
+	
+	public Node find(int v) {
+		Node current = this;
+		while(current != null) {
+			if(current.value == v){
+				return current;
+			}
+			current = v < current.value ? current.left :  current.right;
+		}
+		return null;
+	}
+}
